531700f695f4ca368868924b4b832f4be3d0c242
chafikdeveloper <chafikdevelopper@gmail.com>
2025-08-04 20:36:17 +0100

    filtering and pagination

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { Link, router } from '@inertiajs/vue3';
+
+defineProps({
+    listing: Object,
+});
+
+const params = route().params;
+
+const selectUser = (id) => {
+    router.get(route('home'), {
+        user_id: id,
+        search: params.search,
+        tag: params.tag
+    })
+}
+
+const selectTags = (tag) => {
+    router.get(route('home'), {
+        user_id: params.user_id,
+        search: params.search,
+        tag
+    })
+}
+</script>
+
+<template>
+    <div
+        class="bg-white rounded-lg shadow-lg overflow-hidden dark:bg-slate-800 h-full flex flex-col justify-between"
+    >
+        <div>
+            <!-- Image -->
+            <Link href="">
+              <img
+                    :src="
+                        listing.image
+                            ? `/storage/${listing.image}`
+                            : '/storage/images/listing/default.png'
+                    "
+                    class="w-full h-48 object-cover object-center bg-slate-300"
+                    alt=""
+                />
+               <!-- <img src="/storage/app/public/images/listing/jeep.jpg" alt=""> -->
+            </Link>
+
+            <!-- Title & user -->
+            <div class="p-4">
+                <h3 class="font-bold text-xl mb-2">
+                    {{ listing.title.substring(0, 40) }}...
+                </h3>
+
+                <p>
+                    Listed on
+                    {{ new Date(listing.created_at).toLocaleDateString() }} by
+                    <button @click="selectUser(listing.user.id)" class="text-link">{{ listing.user.name }}</button>
+                </p>
+            </div>
+        </div>
+
+        <!-- Tags -->
+         <div v-if="listing.tags" class="flex items-center gap-3 px-4 pb-4">
+          <div v-for="(tag, index) in listing.tags.split(',')" :key="index">
+            <button @click="selectTags(tag)" class="bg-slate-500 text-white px-2 py-px rounded-full hover:bg-slate-700 dark:hover:bg-slate-900">{{ tag }}</button>
+          </div>
+         </div>
+    </div>
+</template>
